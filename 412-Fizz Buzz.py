787cdd752f0284832d37e7f30df664d6d2239b17
dswe284 <dswe284@gmail.com>
2020-12-13 11:15:43 -0800

    412

--- 412-Fizz Buzz.py
+++ 412-Fizz Buzz.py
@@ -74,3 +74,27 @@ class Solution(object):
 
 
 
+# FizzBuzz  Solution 3
+
+class Solution:
+    def fizzBuzz(self,n):
+        ans = []
+        #Dictionary to store all fizzbuzz mappings
+        fizz_buzz_dict = {3:"Fizz", 5:"Buzz"}
+        for num in range(1, n+1):
+            num_ans_str = ""
+            for key in fizz_buzz_dict.keys():
+                if num % key == 0:
+                    num_ans_str += fizz_buzz_dict[key]
+            if not num_ans_str:
+                num_ans_str = str(num)
+            ans.append(num_ans_str)
+
+        return ans
+
+
+# Tiem: O(a)
+# Space: O(1)
+
+
+
