758d7ea614c4e65f402ebb6291782728178a6245
dswe284 <dswe284@gmail.com>
2020-12-14 22:05:20 -0800

    21

--- 21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists.py
@@ -1,6 +1,7 @@
 # Leetcode 21 Merge Two Sorted Lists
 # Easy 12/13/20 
 
+# Solution 1
 class Node(val):
     def __init__(self,val):
         self._val = val
@@ -28,4 +29,47 @@ class Solution(object):
 # Time: O(a)
 # Space: O(1)
 
+
+
+# Solution 2
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        if not l1 or not l2:
+            return l1 or l2
+        head = small = l1 if l1.val < l2.val else l2
+
+        big = l1 if l1.val >= l2.val else l2
+        pre = None
+
+        while big:
+            if not small:
+                pre = small
+                small = small.next
+            else:
+                tmp = big.next
+                pre.next = big
+                pre = big
+                big.next = small
+                big - tmp
+
+        return head
+
+
+
+
+# Solution 3
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+            
 
