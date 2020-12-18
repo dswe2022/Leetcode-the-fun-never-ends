13ca4be68cadc05c001c61530b71495fae3db48b
dswe284 <dswe284@gmail.com>
2020-12-18 14:00:28 -0800

    387

---
+++ 387-FirstUniqueCharacterInAString.py
@@ -0,0 +1,19 @@
+# Leetcode 387 First Unique Character in a String
+# Easy 12/18/20 
+
+# Given an a by b 2d grid map of "1" s (land) and "0" (water), return the number of islands.
+
+from collections import Counter
+
+def firstUniqChar(s):
+    count = Counter(s)
+    #the index
+    for idx, ch in enumerate(s):
+        if count[ch] == 1:
+            return idx
+    
+    return -1
+
+
+# Time: O(a)
+# Space: O(1)
