e2e52513028f96af24ebc61958328cd5051fb6d3
dswe284 <dswe284@gmail.com>
2021-01-14 14:45:05 -0800

    322

--- 322-CoinChange.py
+++ 322-CoinChange.py
@@ -43,31 +43,7 @@
 #     1 <= coins[i] <= 231 - 1
 #     0 <= amount <= 104
 
-# Solution 1
-
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount +1)
-        dp[0] = 0
-        def dfs(a):
-            if a < 0:
-                return a
-            for c in coins:
-                res = dfs(a - c)
-                if res >= 0:
-                    dp[a] = min(dp[a],  1 + res)
-            return dp[a] if dp[a] < amount + 1 else -1
-        return dfs(amount)
-
-        
-
-
-
-
-
-
-
-# Solution 2 Bottom-up Dyanamic Programming approach
+# Solution 1 Bottom-up Dyanamic Programming approach
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         rs = [amount + 1] * (amount + 1)
@@ -84,3 +60,58 @@ class Solution:
 
 # Time: O(a*b), a is number of elements -1 and b is number of elements in coins.
 # Space: O(k), k is number of elements in coin.
+
+
+
+
+# Solution 2
+# Top-Down recursive with cacheing
+# Time limit exceeded.
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount +1)
+        dp[0] = 0
+        def dfs(a):
+            if a < 0:
+                return a
+            for c in coins:
+                res = dfs(a - c)
+                if res >= 0:
+                    dp[a] = min(dp[a],  1 + res)
+            return dp[a] if dp[a] < amount + 1 else -1
+        return dfs(amount)
+
+
+
+
+
+
+
+# Solution 3 2-D state transition equation:
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        "Method 0, self done, 0-1 knapsack"
+        comp = lambda x, y: y if x == 0 else min(x, y)
+        coins.sort(reverse=True)
+        limit = amount + 1
+        dp = [0 for _ in range(limit)]
+        dp[0] = 1
+        for coin in coins:
+            for col in range(coin, limit):
+                if dp[col - coin] > 0:
+                    dp[col] = comp(dp[col], 1 + dp[col - coin])
+        return dp[-1] - 1
+
+# If we ignore the zeros in dp array, 2-D state transition equation (row = coin):
+# dp[row][col] = min(dp[row][col], dp[row][col - coin] + 1
+
+# Then I truncated the 2-D equation to 1-D because the calculation is done row by row, col by col.
+# So the 1-D state transition equation:
+# dp[col] = min(dp[col], dp[col - coin] + 1)
+
+# Now that the num in dp array means the amount of objects that is already in the knapsack. Intuitively, it should starts from zero and it adds up later. However, in that way, min(0, anything) = 0. So I generated the comp function to solve this problem.
+
+# In the beginning, when we have 0 items in the knapsack, we have 0 weight. To distinguish from all other states, I set the start state (0 item, 0 weight) to have 1 item with 0 weight. In this case, all over the calculation, the valid dp state would always have item number bigger than 0. Remember to remove this virtual item from the knapsack in the end.
+
+# Also, didn't think it through why sorting the coins would make code faster.
