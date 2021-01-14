c4af4fa90f3edd2c30d2cc3acba4e4cf605c7f1a
dswe284 <dswe284@gmail.com>
2021-01-14 15:15:00 -0800

    146

--- 146-LRUCache.py
+++ 146-LRUCache.py
@@ -43,6 +43,161 @@
 #     At most 3 * 104 calls will be made to get and put.
 
 
+# We're asked to implement the structure which provides the following operations in O(1)\mathcal{O}(1)O(1) time :
+
+#     Get the key / Check if the key exists
+
+#     Put the key
+
+#     Delete the first added key
+
+# The first two operations in O(1)\mathcal{O}(1)O(1) time are provided by the standard hashmap, and the last one - by linked list.
+
+# We are asked to implemtn the structure which provides the following operations in O(1) time.
+
+# Get the key/check if the key exists
+# Put the key
+# Delete the first added key
+
+# Solution 1
+# Use a OrderedDict from collections.
+
+from collections import OrderedDict
+class LRUCache(OrderedDict):
+
+    def __init__(self, capacity):
+        # Type capacity: int
+        self.capacity = capacity
+    
+    def get(self,key):
+        if key not in self:
+            return -1
+        self.move_to_end(key)
+        return self[key]
+
+    def put(self, key, value):
+        if key in self:
+            self.move_to_end(key)
+        self[key] = value
+        if len(self) > self.capacity:
+            self.popitem(last = False)
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key, value)
+
+# T: O(1) both for put and get since all operations with ordered dictionary: get/in, 
+# /set/move_to_end/popitem (get/containsKey/put/remove) are done in constant time.
+
+# S: O(capacity).
+
+
+# Solution 2
+# Use a hashmap and Doubly linked list.
+# classic solution
+
+
+# IMPORTANT: The reason why double linked list is used is that the node can remove itself without other reference. 
+
+# It takes constant time to add and remove nodes from the head or tail.
+# One particularity about the double linked list implemented here is that there are pseudo head and pseudo tail to 
+# mark the boundary.
+
+
+
+# Declare the doubly linked list node first.
+class DLinkedNode():
+    def __init__(self):
+        self.key = 0 
+        self.value = 0
+        self.prev = None
+        self.next = None
+    
+
+class LRUCache():
+    def _add_node(self,node):
+        # Always add the new node right after head.
+        node.prev = self.head
+        node.next = self.head.next
+
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove_node(self.node):
+        # Remove an exisiting node from the linked list.
+        prev = node.prev
+        new = node.next
+        prev.next = new
+        new.prev = prev
+
+
+    def _move_to_head(self, node):
+        # Move certain node in between to the head.
+        self._remove_node(node)
+        self._add_node(node)
+
+    def _pop_tail(self):
+        # Pop the current tail
+        res = self.tail.prev
+        self._remove_node(res)
+        return res
+
+    def __init__(self,capacity):
+        # type capacity: int
+        self.cache = {}
+        self.size = 0
+        self.capcity = capacity
+        self.head = DLinkedNode()
+        self.tail = DLinkedNode()
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+
+    def get(self, key):
+        # type key: int
+        # return type: int
+        node = self.cache.get(key, None)
+        if not node:
+            return -1
+        
+        # move the accessed node to the head
+        self._move_to_head(node)
+
+        return node.value
+
+    def put(self,key, value):
+        node = self.cache.get(key)
+
+        if not node:
+            newNode = DLinkedNode()
+            newNode.key = key
+            newNode.value = value
+
+            self.cache[key] = newNode
+            self._add_node(newNode)
+
+            self.size += 1
+
+            if self.size > self.capacity:
+                # pop the tail 
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+                self.size -= 1
+        else:
+            # update the value
+            node.value = value
+            self._move_to_head(node)
+
+
+
+# T: O(1)
+# S: O(capacity)
+
+
+# Solution 3
+
 class LRUCache(object):
 
     def __init__(self, capacity):
