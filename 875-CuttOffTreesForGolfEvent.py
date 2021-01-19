0ca0a502de13a1bcc98eaabc18ac8d9e95d8d65a
dswe284 <dswe284@gmail.com>
2021-01-19 01:59:47 -0800

    543

--- 875-CuttOffTreesForGolfEvent.py
+++ 875-CuttOffTreesForGolfEvent.py
@@ -16,3 +16,45 @@
 
 # You are guaranteed that no two trees have the same height, and there is at least one tree needs to be cut off.
 
+
+
+from collections import defaultdict
+import heapq
+
+class Solution:
+    def cutOffTree(self, forest: List[List[int]]) -> int:
+        N, M, MAX = len(forest), len(forest[0]) if len(forest) else 0, float('inf')
+        
+        def get(frX: int, frY: int, toX: int, toY: int) -> int: # get shortest
+            if not (0 <= frX < N and 0 <= toX < N and 0 <= frY < M and 0 <= toY < M):
+                return MAX
+            if not forest[frX][frY]:
+                return MAX
+            
+            # queue = [ (estimated total length, estimated remaining length, distance from source, source X, source Y) ]
+            queue = [(abs(frX-toX)+abs(frY-toY), abs(frX-toX)+abs(frY-toY), 0, frX, frY  )]
+            shortest = defaultdict(lambda: MAX)
+            shortest[frX, frY] = 0
+            
+            while len(queue):
+                est, remain, d, x, y = heapq.heappop(queue)
+                if (x, y) == (toX, toY):
+                    return d
+                for a, b in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+                    remaining = abs(a-toX) + abs(b-toY)
+                    if 0 <= a < N and 0 <= b < M and forest[a][b] and shortest[a, b] > d+1:
+                        shortest[a, b] = d+1
+                        heapq.heappush(queue, (remaining+d+1, remaining, d+1, a, b) )
+            
+            return MAX
+        
+        path = sorted( (forest[x][y], x, y) for x in range(N) for y in range(M) if forest[x][y] > 1 )
+
+        ans, frX, frY = 0, 0, 0
+        for v, x, y in path:
+            s = get(frX, frY, x, y)
+            if s < MAX:
+                ans, frX, frY = ans + s, x, y
+            else:
+                return -1
+        return ans
