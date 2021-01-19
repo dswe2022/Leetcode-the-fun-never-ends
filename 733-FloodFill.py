333dfaff06d1fa416a92e1898e0abf998b7ee8b5
sfcoder88 <forkhub284@gmail.com>
2021-01-19 01:16:14 -0800

    733

--- 733-FloodFill.py
+++ 733-FloodFill.py
@@ -26,3 +26,40 @@
 # The length of image and image[0] will be in the range [1, 50].
 # The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
 # The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+
+# Solution 1
+
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        # Calcuate the length of rows and columns.
+        R = len(image)
+        C = len(image[0])
+        
+        # check the color and set the current color from the origin coord.
+        color = image[sr][sc]
+        # if color of origin coord is newColor, just return image.
+        if color == newColor:
+            return image
+        
+        # No return function. Just flip the numbers.
+        # Put the dfs function inside floodFill function so you don't 
+        # have to keep copying the image board. Because the same image board
+        # is in same scope, you don't have to keep passing it into dfs().
+        # Also, you don't have to keep passing in the row and col bounds.
+        def dfs(row,col):
+            if image[row][col] == color:
+                image[row][col] = newColor
+                if row >= 1:
+                    dfs(row-1, col)
+                if row +1 < R:
+                    dfs(row +1, col)
+                if col >= 1:
+                    dfs(row, col-1)
+                if col+1 < C:
+                    dfs(row, col +1)
+        
+        dfs(sr,sc)
+        return image
+        
+        
