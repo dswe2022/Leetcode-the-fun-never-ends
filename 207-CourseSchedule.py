702633adb8d8e1c87303fec321f01eabcbac068b
dswe284 <dswe284@gmail.com>
2021-01-19 18:06:18 -0800

    207

--- 207-CourseSchedule.py
+++ 207-CourseSchedule.py
@@ -36,6 +36,36 @@
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # map each course to prereq list
+        preMap = { i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+            
+        # visitSet = all courses along the curr DFS path
+        visitSet = set()
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if preMap[crs] == []:
+                return True
+            
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+            visitSet.remove(crs)
+            preMap[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+        return True
+
+
+
+# Solution 2
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         res = []
         graph = [[] for _ in range(numCourses)]
         visit = [0 for _ in range(numCourses)]
@@ -67,7 +97,7 @@ class Solution:
 
 
 
-# Solution 2
+# Solution 3
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
@@ -99,7 +129,7 @@ class Solution:
         return True
                 
 
-# Solution 3
+# Solution 4
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
