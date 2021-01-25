69d4f24d68853e9ebbe4a86014d7de0d6c2b40be
dswe284 <dswe284@gmail.com>
2021-01-24 23:42:17 -0800

    103

--- 103-BinaryTreeZigzagLevel.py
+++ 103-BinaryTreeZigzagLevel.py
@@ -27,27 +27,106 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# Solution Breadth First Search
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+
 class Solution:
-    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-        res = []
-        if not root:
-            return res
-        q = [root]
-        reverse = False
-        while q:
-            new_q = []
-            if reverse:
-                res.append([n.val for n in q][::-1])
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        ret = []
+        level_list = deque()
+        if root is None:
+            return []
+        # start with the level 0 with a delimiter
+        node_queue = deque([root, None])
+        is_order_left = True
+
+        while len(node_queue) > 0:
+            curr_node = node_queue.popleft()
+
+            if curr_node:
+                if is_order_left:
+                    level_list.append(curr_node.val)
+                else:
+                    level_list.appendleft(curr_node.val)
+
+                if curr_node.left:
+                    node_queue.append(curr_node.left)
+                if curr_node.right:
+                    node_queue.append(curr_node.right)
             else:
-                res.append([n.val for n in q])
-            reverse = not reverse
-            for node in q:
-                if node.left:
-                    new_q.append(node.left)
-                if node.right:
-                    new_q.append(node.right)
-            q = new_q
-        return res
+                # we finish one level
+                ret.append(level_list)
+                # add a delimiter to mark the level
+                if len(node_queue) > 0:
+                    node_queue.append(None)
+
+                # prepare for the next level
+                level_list = deque()
+                is_order_left = not is_order_left
+
+        return ret
+
+
+
+
+# Solution 2 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+
+class Solution:
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+
+        results = []
+        def dfs(node, level):
+            if level >= len(results):
+                results.append(deque([node.val]))
+            else:
+                if level % 2 == 0:
+                    results[level].append(node.val)
+                else:
+                    results[level].appendleft(node.val)
+
+            for next_node in [node.left, node.right]:
+                if next_node is not None:
+                    dfs(next_node, level+1)
+
+        # normal level order traversal with DFS
+        dfs(root, 0)
+
+        return results
+
 
 # T: O(a*a)
 # S: O(a)
