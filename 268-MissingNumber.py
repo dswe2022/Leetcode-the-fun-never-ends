c38ba860c5c1db73328040a98220c2994e6b9f4a
dswe284 <dswe284@gmail.com>
2021-01-24 23:27:00 -0800

    387

--- 268-MissingNumber.py
+++ 268-MissingNumber.py
@@ -4,18 +4,18 @@
 # Solution A
 def missingNumber(nums):
     nums.sort()
-    # Ensure that n is at the last index
     if nums[-1] != len(nums):
-        return != len(nums)
-    # Ensure that 0 is at the first index
+        return len(nums)
+
     elif nums[0] != 0:
         return 0
-    # If we get have, then the missing number
-    # is on the range(0,n)
+    
     for i in range(1, len(nums)):
-        expected_num = num[i-1] +1
-        if nums[i] != expected - num:
+        expected_num = nums[i-1] + 1
+        if nums[i] != expected_num:
             return expected_num
+
+            
 
 
 # Time: O(a *log(a))
--- 387-FirstUniqueCharacterInAString.py
+++ 387-FirstUniqueCharacterInAString.py
@@ -17,3 +17,19 @@ def firstUniqChar(s):
 
 # Time: O(a)
 # Space: O(1)
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        # build hash map : character and how often it appears
+        count = collections.Counter(s)
+        
+        # find the index
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx     
+        return -1
